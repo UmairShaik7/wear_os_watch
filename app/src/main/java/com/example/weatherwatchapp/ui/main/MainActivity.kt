8ae58135cfae47fd4fda4ef4434bbd554a730127
M, Hariprasad <haripm@deloitte.com>
2020-07-30 03:01:34 +0530

    UI for weather app

--- app/src/main/java/com/example/weatherwatchapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherwatchapp/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.weatherwatchapp.ui.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -14,11 +16,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewBinding.viewmodel = mainActivityViewModel
-        mainActivityViewModel.fetchWeather(CityCode.HYDERABAD.cityCode)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        viewBinding.bFirstRegion.setOnClickListener {
+            progressBar?.visibility = View.VISIBLE
+            mainActivityViewModel.fetchWeather(CityCode.BANGALORE.cityCode)
+        }
+
+        viewBinding.bSecondRegion.setOnClickListener {
+            progressBar?.visibility = View.VISIBLE
+            mainActivityViewModel.fetchWeather(CityCode.HYDERABAD.cityCode)
+        }
+
         mainActivityViewModel.weatherValue.observe(this, Observer {
-            viewBinding.titleTemplate.text=it.main.temp.toString()
+            progressBar?.visibility = View.GONE
+
+            viewBinding.tvWeatherResult.text = baseContext.getString(
+                R.string.weather_result, it.main.temp.toString(), it.weather?.get(
+                    index = 0
+                )?.main.toString()
+            )
         })
     }
 }
