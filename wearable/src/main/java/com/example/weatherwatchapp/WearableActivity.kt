14c3b8c5190430f27a6aa307bdd6e4d873ed6a77
M, Hariprasad <haripm@deloitte.com>
2020-08-18 00:58:04 +0530

    Updated UI in Watch and Phone modules

--- wearable/src/main/java/com/example/weatherwatchapp/WearableActivity.kt
+++ wearable/src/main/java/com/example/weatherwatchapp/WearableActivity.kt
@@ -6,14 +6,15 @@ import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.wearable.MessageClient.OnMessageReceivedListener
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.Wearable
-import kotlinx.android.synthetic.main.activity_main.text
+import kotlinx.android.synthetic.main.activity_main.temperature
+import kotlinx.android.synthetic.main.activity_main.lottieWeatherAnimation
 
-class MainActivity : FragmentActivity(), OnMessageReceivedListener{
+class WearableActivity : FragmentActivity(), OnMessageReceivedListener{
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       }
+    }
 
     override fun onResume() {
         super.onResume()
@@ -23,10 +24,23 @@ class MainActivity : FragmentActivity(), OnMessageReceivedListener{
     override fun onPause() {
         super.onPause()
         Wearable.getMessageClient(this).removeListener(this)
-       }
+    }
 
     override fun onMessageReceived(event: MessageEvent) {
-        text.text= String(event.data)
+        temperature.text= getString(R.string.temperature, String(event.data))
+
+        // TODO After UI discussion
+        if (String(event.data) in "20".."22") {
+            lottieWeatherAnimation.apply {
+                setAnimation("sunny_day.json")
+                playAnimation()
+            }
+        } else {
+            lottieWeatherAnimation.apply {
+                setAnimation("raining.json")
+                playAnimation()
+            }
+        }
         Toast.makeText(this,"Received",Toast.LENGTH_SHORT).show()
     }
 }
